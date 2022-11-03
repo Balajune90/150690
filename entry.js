10b0852c4b19918256dc939fc359fcb32aa06409
Balajune90 <balajune90@gmail.com>
2022-11-03 23:35:33 +0530

    Update entry.js

--- entry.js
+++ entry.js
@@ -1,1 +1,16 @@
-console.log("Hello NodeJs from WebFramework")
+import express from 'express'
+const app=express();
+
+app.get('/restapi',(req,res)=>{
+    res.send(JSON.stringify({message:"Welcome to Node JS",
+    articleData:{
+        articleName: "How to send JSON response from NodeJS from Framework using visual Studio",
+        category:"NodeJS",
+        status: "WIP"
+    },
+    endingMessage:"Good Start of Express"  }))
+    
+})
+app.listen(9999,()=>{
+    console.log("Server is listening")
+});
